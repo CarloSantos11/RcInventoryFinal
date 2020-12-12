b57dd02345bcb6e89e20d152c8484a51f30821a3
ayus <l65.fread@gmail.com>
2020-12-12 09:56:25 +0545

    Worked on the Inventory.java class to make methods for adding items.

--- src/com/devnom/model/Shell.java
+++ src/com/devnom/model/Shell.java
@@ -1,7 +1,15 @@
 package com.devnom.model;
 
 public class Shell {
-    String type; // I have more than a binary option here
-                 // Possibly use an enum?
-                 // Same design decision as the frame
+    String type;
+    double price;
+
+    public Shell(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    // I have more than a binary option here
+    // Possibly use an enum?
+    // Same design decision as the frame
 }
--- src/com/devnom/model/Wheel.java
+++ src/com/devnom/model/Wheel.java
@@ -1,4 +1,11 @@
 package com.devnom.model;
 
 public class Wheel {
+    boolean isWide;
+    double price;
+
+    public Wheel(boolean isWide, double price) {
+        this.isWide = isWide;
+        this.price = price;
+    }
 }
